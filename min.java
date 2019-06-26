566fe87a43724694ff73b92d5efa14b4800ce1fc
PreethaHarini <42317155+PreethaHarini@users.noreply.github.com>
2019-06-26 11:34:08 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class min
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        //int min=a[1];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        
+        System.out.println(a[0]);
+    }
+}
